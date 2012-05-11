5a73a34b9e0ea6c65bd2d2594c7186ea4f892e65
administrator <administrator@nfeed.org>
2012-05-12 00:33:04 +0200

    GUI

--- MCMS/Form1.Designer.cs
+++ MCMS/Form1.Designer.cs
@@ -28,15 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.SuspendLayout();
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(527, 226);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "MCMS";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
