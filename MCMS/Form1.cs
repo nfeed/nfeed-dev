5c36f325d415014e1353091b40f50c05f0a5739e
teyhouse <teyhouse@nfeed.org>
2012-05-12 00:22:37 +0200

    add

--- MCMS/Form1.cs
+++ MCMS/Form1.cs
@@ -19,6 +19,8 @@ namespace MCMS
         private void Form1_Load(object sender, EventArgs e)
         {
             MessageBox.Show("lol");
+            MessageBox.Show("mem");
         }
+        
     }
 }
