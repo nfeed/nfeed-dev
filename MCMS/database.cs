cc696a173954de91a5b6e2fe5bdd6c6bafcaaab8
administrator <administrator@nfeed.org>
2012-05-13 00:06:30 +0200

    trz

---
+++ MCMS/database.cs
@@ -0,0 +1,19 @@
+﻿namespace MCMS
+{
+    public class database
+    {
+        //Felder
+        private string host;
+        private string user;
+        private string password;
+        private string database;
+
+        public database(string host, string user, string password, string database)
+        {
+            this.host = host;
+            this.user = user;
+            this.password = password;
+            this.database = database;
+        }
+    }
+}
